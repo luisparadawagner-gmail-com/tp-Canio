36a50e5fbf557a62fad9dd9bf9f2a600ef144044
Sofia <caniosofia@gmail.com>
2020-04-19 19:17:12 -0300

    creacion de formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { AltaAlumnosComponent} from './alta-alumnos/alta-alumnos.component';
+import { PersonaComponent } from './reactivos/persona.component';
 
 @NgModule({
-  declarations: [ AppComponent, AltaAlumnosComponent ],
-  imports: [BrowserModule,AppRoutingModule],
+  declarations: [ AppComponent, AltaAlumnosComponent, PersonaComponent ],
+  imports: [BrowserModule,AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/reactivos/persona.component.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+
+@Component({
+    selector: 'app-reactive-persona',
+    templateUrl: './persona.component.html'
+})
+
+export class PersonaComponent{
+    
+    personaForm = new FormGroup({
+        nombre : new FormControl(''),
+        apellido : new FormControl(''),
+        dni : new FormControl(''),
+        fechaNacimiento : new FormControl(''),
+        edad : new FormControl('')
+    })
+
+    guardarPersona() {
+        debugger;
+        this.personaForm.value;
+        console.warn(this.personaForm.value)
+    }
+}
